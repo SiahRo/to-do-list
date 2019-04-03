5f7014d51af59d4bb9d7c5c433905b0b787798e5
Jesiah Roche <jesiah.roche@gmail.com>
2019-04-02 22:08:15 -0400

    more style

--- js/master.js
+++ js/master.js
@@ -7,16 +7,18 @@ $("#addButton").click(function() {
 
     var checkBox = $("<input/>", {type:"checkbox"})
     var newListItem = $("<li/>", {class:"list-item", text: inputBox.val()});
+    var line = $("<hr/>");
     list.append(newListItem);
     inputBox.val("");
     newListItem.prepend(checkBox);
+    newListItem.append(line);
 
     checkBox.on("click", function() {
       $(this).parent().remove();
       $("#toast").css("display", "block");
       setTimeout(function(){
         $("#toast").hide();
-      }, 5000);
+      }, 3000);
 
     });
   }
